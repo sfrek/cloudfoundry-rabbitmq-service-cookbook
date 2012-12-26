02f1121bf22023bc15bbe59a1fe2cea99e1701f6
Andrea Campi <andrea.campi@zephirworks.com>
2012-12-27 00:31:06 +0100

    FC005: Avoid repetition of resource declarations

--- recipes/node.rb
+++ recipes/node.rb
@@ -1,18 +1,10 @@
 include_recipe "cloudfoundry-rabbitmq-service::install"
 
-directory node['cloudfoundry_rabbitmq_service']['node']['base_dir'] do
-  owner node['cloudfoundry']['user']
-  mode  "0755"
-end
-
-directory node['cloudfoundry_rabbitmq_service']['node']['db_logs_dir'] do
-  owner node['cloudfoundry']['user']
-  mode  "0755"
-end
-
-directory node['cloudfoundry_rabbitmq_service']['node']['instances_dir'] do
-  owner node['cloudfoundry']['user']
-  mode  "0755"
+%w(base_dir db_logs_dir instances_dir).each do |dir|
+  directory node['cloudfoundry_rabbitmq_service']['node'][dir] do
+    owner node['cloudfoundry']['user']
+    mode  "0755"
+  end
 end
 
 cloudfoundry_service_component "rabbitmq_node" do
