1ce9f25a41d47638e7714bb17dbfe827ca719061
Andrea Campi <andrea.campi@zephirworks.com>
2012-12-28 01:12:47 +0100

    Add proper unit tests for the gateway.

---
+++ test/kitchen/cookbooks/cloudfoundry-rabbitmq-service_test/files/default/tests/minitest/gateway_test.rb
@@ -0,0 +1,55 @@
+require File.expand_path('../support/helpers', __FILE__)
+
+begin
+  require 'pg'
+rescue LoadError
+  Chef::Log.warn "Can not load gem 'pg', trying to continue anyway"
+end
+
+describe 'cloudfoundry-rabbitmq-service::gateway' do
+  include Helpers::CFServiceRabbitMQTest
+
+  before do
+    # Give the service some time to register with the cloud_controller;
+    # plus sometimes the cloud_controller is really slow to start.
+    sleep 75
+  end
+
+  it 'registers a service in the cloud_controller DB' do
+    db = connect('cloud_controller', 'cloudfoundry', 'cloudfoundry')
+    res = db.query("select * from services")
+    res.count.must_equal 1
+
+    row = res[0]
+    row['provider'].must_be_nil
+    row['token'].wont_be_nil
+    row['binding_options'].must_be_nil
+    row['acls'].must_be_nil
+    row['active'].must_equal   "t"
+    row['url'].must_match      %r{^http://10\.0\.2\.15:\d+$}
+
+    row['label'].must_equal    "rabbitmq-2.4"
+    row['name'].must_equal     "rabbitmq"
+    row['version'].must_equal  "2.4"
+    row['timeout'].must_equal  "15"
+
+    tags = YAML.load(row['tags'])
+    tags.must_equal ['rabbitmq', 'rabbitmq-2.4', 'message-queue', 'amqp']
+
+    plans = YAML.load(row['plans'])
+    plans.must_equal ["free"]
+    row['plan_options'].must_be_nil
+    row['default_plan'].must_equal "free"
+  end
+
+protected
+  def connect(dbname, user, password)
+    @db ||= ::PGconn.new(
+      :host => '127.0.0.1',
+      :port => 5432,
+      :dbname => dbname,
+      :user => user,
+      :password => password
+    )
+  end
+end
