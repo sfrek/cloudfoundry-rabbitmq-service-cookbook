a5fedfa622ddb4c47feb8ab9a1aba4c316d6ec68
Andrea Campi <andrea.campi@zephirworks.com>
2013-01-01 23:08:56 +0100

    Release 1.0.1 with updated dependencies.

--- metadata.rb
+++ metadata.rb
@@ -3,12 +3,11 @@ maintainer_email "andrea.campi@zephirworks.com"
 license          "Apache 2.0"
 description      "Installs/Configures cloudfoundry-rabbitmq-service"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.0.1"
 
 %w( ubuntu ).each do |os|
   supports os
 end
 
-%w( cloudfoundry cloudfoundry_service ).each do |cb|
-  depends cb
-end
+depends "cloudfoundry"
+depends "cloudfoundry_service", "~> 1.1.4"
